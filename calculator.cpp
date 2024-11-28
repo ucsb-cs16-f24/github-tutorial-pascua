8803b5efb1cb44495380aba4a0e635675bc0a580
Jarek Pascua <jarek@csilvm-10.cs.ucsb.edu>
2024-11-28 01:11:22 -0800

    Fixed divide by 0 errors.

--- calculator.cpp
+++ calculator.cpp
@@ -30,6 +30,12 @@ int main() {
             cout << "Result: " << result << endl;
             break;
         case '/':
+            if (num2 == 0)
+            {
+                cerr << "Error: Division by 0!\n";
+                exit(1);
+            }
+            
             result = num1 / num2;  // No check for dividing by 0
             cout << "Result: " << result << endl;
             break;
